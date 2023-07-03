74fdd13d2be54011a79f8277c1f6aaf8d766e4d8
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2023-07-03 22:23:55 +0530

    fix: remove unused lines

--- src/main/java/com/paritosh/pranjal/entity/User.java
+++ src/main/java/com/paritosh/pranjal/entity/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
