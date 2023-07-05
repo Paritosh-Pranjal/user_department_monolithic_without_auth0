6f094f379dfba8a5375a29893c9d39606c1b244b
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2023-07-05 12:15:33 +0530

    fix: remove unused import

--- src/main/java/com/paritosh/pranjal/UserDepartmentApplication.java
+++ src/main/java/com/paritosh/pranjal/UserDepartmentApplication.java
@@ -2,10 +2,9 @@ package com.paritosh.pranjal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class UserDepartmentApplication {
+class UserDepartmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserDepartmentApplication.class, args);
