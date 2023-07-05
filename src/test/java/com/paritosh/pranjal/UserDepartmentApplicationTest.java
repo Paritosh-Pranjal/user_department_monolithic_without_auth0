e0efe370e6cddd7d86921fb2194d0ed9a3fe7fdd
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2023-07-05 11:56:53 +0530

    fix: fix the annotation

--- src/test/java/com/paritosh/pranjal/UserDepartmentApplicationTest.java
+++ src/test/java/com/paritosh/pranjal/UserDepartmentApplicationTest.java
@@ -3,7 +3,6 @@ package com.paritosh.pranjal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class UserDepartmentApplicationTest {
 
     @Test
