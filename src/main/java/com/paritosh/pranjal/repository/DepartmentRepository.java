47ba0dc20f26dbf6c96126b2b10944831f5821a8
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2023-07-06 15:31:28 +0530

    fix: make master clear

--- src/main/java/com/paritosh/pranjal/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paritosh.pranjal.repository;
-
-import com.paritosh.pranjal.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-}
